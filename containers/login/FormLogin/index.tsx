f0747e679fa2c27b8015adff5a9f963adbd718e1
thien-lb <dtthien@landbridge.vn>
2023-11-27 12:46:52 +0700

    test hexabase sdk

--- containers/login/FormLogin/index.tsx
+++ containers/login/FormLogin/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { loginSchema } from '@/common/form-schemas';
-import ButtonComponent from '../../../components/atoms/buttons';
-import FormItem from '@/components/atoms/form-items/FormItem';
-import { PasswordInput } from '@/components/atoms/inputs/PasswordInput';
-import TextInput from '@/components/atoms/inputs/TextInput';
-import useAuth from '@/hooks/useAuth';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/router';
-import { FormProvider, useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
-import Cookies from 'js-cookie';
-import { APP_ROUTES } from '@/common/constants/routes';
-import { COOKIES_KEY } from '@/common/constants/cookie';
+import React from "react";
+import { loginSchema } from "@/common/form-schemas";
+import ButtonComponent from "../../../components/atoms/buttons";
+import FormItem from "@/components/atoms/form-items/FormItem";
+import { PasswordInput } from "@/components/atoms/inputs/PasswordInput";
+import TextInput from "@/components/atoms/inputs/TextInput";
+import useAuth from "@/hooks/useAuth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { FormProvider, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import Cookies from "js-cookie";
+import { APP_ROUTES } from "@/common/constants/routes";
+import { COOKIES_KEY } from "@/common/constants/cookie";
+import { HexabaseClient, HexabaseSQL, Item } from "@hexabase/hexabase-js";
 
 const FormLogin: React.FC = () => {
   const {
@@ -22,8 +23,8 @@ const FormLogin: React.FC = () => {
   const methods = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
   });
 
@@ -37,13 +38,49 @@ const FormLogin: React.FC = () => {
   };
 
   const onError = (error: any) => {
-    toast.error(error?.data?.message || 'Sever error');
+    toast.error(error?.data?.message || "Sever error");
   };
 
-  const onSubmit = (values: any) => {
+  const onSubmit = async (values: any) => {
+    console.log(values);
+    const a = new HexabaseClient();
+    const result = await a.login({
+      email: "hexabase_email",
+      password: "hexabase_pass",
+    });
+    console.log(result);
+    // const value = await a.currentWorkspace?.projectsAndDatastores();
+
+    // const b = new HexabaseSQL();
+    // b.projectId = "65582087baeaf8d6328c49b9";
+    // b.datastoreId = "6558260e245accaeb79d5fa0";
+
+    const client = new HexabaseClient("dev", a.tokenHxb);
+    console.log(await getPrefecturesItems(client));
+
+    // (await a.workspace("6548aec4fda94524d403ce4f").project("65582087baeaf8d6328c49b9"))
+    //   .datastore("6558260e245accaeb79d5fa0")
+    //   .then((item) => {
+    //     console.log(item);
+    //   });
+
+    // console.log(b.query());
+
     // return mutate(values, { onSuccess, onError });
-    Cookies.set(COOKIES_KEY.ACCESS_TOKEN, '1');
-    router.push(APP_ROUTES.HOME);
+    // Cookies.set(COOKIES_KEY.ACCESS_TOKEN, "1");
+    // router.push(APP_ROUTES.HOME);
+  };
+
+  const getPrefecturesItems = async (client: HexabaseClient): Promise<Item[]> => {
+    const workspace = await client.workspace("6548aec4fda94524d403ce4f");
+    const project = await workspace.project("65582087baeaf8d6328c49b9");
+    const datastore = await project.datastore("6558260e245accaeb79d5fa0");
+    const items = await datastore.items({
+      page: 1,
+      per_page: 10,
+      use_display_id: true,
+    });
+    return items;
   };
 
   return (
@@ -60,7 +97,7 @@ const FormLogin: React.FC = () => {
             <ButtonComponent
               variant="out-line"
               text="ログイン"
-            // disabled={isLoading}
+              // disabled={isLoading}
             />
           </div>
         </form>
